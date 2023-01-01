68c1de4b890cbc0621e647aaed1177ce2fa60e49
Haruki Ambai <s1270128@u-aizu.ac.jp>
2023-01-02 03:39:27 +0900

    #update : contants update

--- src/pages/Contacts/Contacts.tsx
+++ src/pages/Contacts/Contacts.tsx
@@ -1,39 +1,50 @@
 import "./Contacts.scss";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import Email from "@mui/icons-material/Email";
-import LinkedIn from "@mui/icons-material/LinkedIn";
-import { Grid, Link } from "@material-ui/core";
+import emailjs from "@emailjs/browser";
+import { Grid } from "@material-ui/core";
+import { Button } from "@mui/material";
 
 const Contacts = () => {
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        `${process.env.REACT_APP_EMAIL_SERVICE_ID}`,
+        `${process.env.REACT_APP_EMAIL_TEMPLATE_ID}`,
+        e.currentTarget,
+        `${process.env.REACT_APP_EMAIL_PUBLIC_KEY}`
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
-    <Grid container className="contact_wrapper">
-      <Grid item>
-        <Link className="contact_content" color="inherit">
-          <Email className="icon" />
-          <h2>haruru898@gmail.com</h2>
-        </Link>
+    <form onSubmit={sendEmail}>
+      <Grid container className="contact_wrapper">
+        <Grid item sm={6} lg={3}>
+          <h2>Name</h2>
+          <input type="text" name="user_name" required />
+        </Grid>
+        <Grid item sm={6} lg={3}>
+          <h2>Email</h2>
+          <input type="email" name="user_email" required />
+        </Grid>
+        <Grid item xs={12} className="text_wrapper">
+          <h2>Message</h2>
+          <textarea required name="message" rows={100} className="text_area" />
+        </Grid>
+        <Grid container xs={12} justifyContent="flex-end">
+          <Button type="submit" variant="contained">
+            Send
+          </Button>
+        </Grid>
       </Grid>
-      <Grid item>
-        <Link
-          href="https://github.com/anbyjap"
-          className="contact_content"
-          color="inherit"
-        >
-          <GitHubIcon className="icon" />
-          <h2>anbyjap</h2>
-        </Link>
-      </Grid>
-      <Grid item>
-        <Link
-          href="https://www.linkedin.com/in/haruki-ambai-76a66025b/"
-          className="contact_content"
-          color="inherit"
-        >
-          <LinkedIn className="icon" />
-          <h2>Haruki Ambai</h2>
-        </Link>
-      </Grid>
-    </Grid>
+    </form>
   );
 };
 
