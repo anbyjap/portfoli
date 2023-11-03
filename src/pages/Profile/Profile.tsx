a0156c64067bcd3672bc50c499804b59203ff8f3
Anbyjap <haruru898@gmail.com>
2023-11-02 19:13:21 -0700

    # feat : skills icons feated

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -2,8 +2,25 @@ import { Email, LinkedIn, GitHub } from "@material-ui/icons";
 import { Link } from "@mui/material";
 import selfPhoto from "../../images/faceimage.png";
 import "./Profile.scss";
+import { SkillsIcon } from "../../components/SkillsIcon";
+import { useState } from "react";
+
+const images = [
+  <SkillsIcon name="React" size={50} />,
+  <SkillsIcon name="TypeScript" size={50} />,
+  <SkillsIcon name="Python" size={50} />,
+  <SkillsIcon name="Docker" size={50} />,
+];
 
 const Profile = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const hoverStyle = isHovered
+    ? {
+        transform: "scale(1.1)", // Example hover style
+        transition: "transform 0.3s",
+      }
+    : {};
   return (
     <div className="profile_wrapper">
       <div className="photo_wrapper">
@@ -11,6 +28,82 @@ const Profile = () => {
       </div>
       <div className="introduction_wrapper fadeInUp">
         <div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              minHeight: "10vh",
+              justifyContent: "center",
+            }}
+          >
+            <div
+              style={{
+                width: "200%",
+                height: "5rem",
+                overflow: "hidden",
+                position: "relative",
+              }}
+            >
+              <div
+                className="animate"
+                style={{
+                  width: "200%",
+                  display: "flex",
+                  alignItems: "center",
+                  height: "5rem",
+                  justifyContent: "space-around",
+                  position: "absolute",
+                  left: 0,
+                  // For the animation, you will need to define this in CSS
+                  // e.g. animation: 'slide 30s infinite linear'
+                }}
+              >
+                {images.map((i, index) => {
+                  return (
+                    <button
+                      key={index}
+                      className="skill-button"
+                      style={{
+                        backgroundColor: "transparent",
+                        borderColor: "transparent",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "flex-start",
+                        width: "30rem",
+                        // For the gap, you will need to apply margin inline or handle in CSS
+                      }}
+                      onMouseEnter={() => setIsHovered(true)}
+                      onMouseLeave={() => setIsHovered(false)}
+                    >
+                      {i}
+                    </button>
+                  );
+                })}
+                {images.map((i, index) => {
+                  return (
+                    <button
+                      key={index}
+                      className="skill-button"
+                      style={{
+                        backgroundColor: "transparent",
+                        borderColor: "transparent",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "flex-start",
+                        width: "30rem",
+                        // For the gap, you will need to apply margin inline or handle in CSS
+                      }}
+                      onMouseEnter={() => setIsHovered(true)}
+                      onMouseLeave={() => setIsHovered(false)}
+                    >
+                      {i}
+                    </button>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+
           <h1>
             Hello! I'm <span>Full Stack Web Developer</span>
           </h1>
