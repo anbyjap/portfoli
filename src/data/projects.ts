f8b5d86a6ead2c59015f30276b4f374b516f6cf0
Anbyjap <haruru898@gmail.com>
2023-11-07 03:42:45 -0800

    # add : cleanAp added

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -10,6 +10,9 @@ import spogocchiImage4 from "../images/spogocchi_4.png";
 import yenn1 from "../images/yenn1.png";
 import yenn2 from "../images/yenn2.png";
 import yenn3 from "../images/yenn3.png";
+import cleanAp1 from "../images/CleanAp/1.png";
+import cleanAp2 from "../images/CleanAp/2.png";
+import cleanAp3 from "../images/CleanAp/3.png";
 import { CarouselItemProps } from "../types";
 
 export const projects: CarouselItemProps[] = [
@@ -22,6 +25,17 @@ export const projects: CarouselItemProps[] = [
     imgSrc: coiasImage,
     projectScreenshots: [coiasImage, coiasImage1, coiasImage2],
     link: "https://web-coias.u-aizu.ac.jp/",
+  },
+  {
+    name: "Yenn",
+    description: `Yenn is an innovative blogging platform that stands at the intersection of simplicity and advanced technology. Initially created as a personal blog, the vision for Yenn is to eventually extend its reach to a broader user base. At its core, Yenn is designed to be extraordinarily user-friendly, ensuring that users can navigate to and read articles with minimal effort and maximum enjoyment.
+    The platform is built on a robust technical framework that combines React and TypeScript for a responsive and dynamic frontend. The backend is powered by Python FastAPI, known for its speed and reliability, while PostgreSQL ensures a stable and secure database. The integration of these technologies results in a seamless, fluid experience for the user.
+    A key feature of Yenn is its streamlined navigation, which allows readers to access content with fewer clicks, promoting a focused and uninterrupted reading experience. While currently serving as a personal blogging space, Yenn is constructed with scalability in mind, laying the groundwork for future expansion and community building.
+    Looking ahead, Yenn aspires to evolve from a personal platform into a vibrant, communal space for diverse stories and perspectives, all while staying true to its foundational principles of simplicity and user-centric design.`,
+    skills: ["React", "TypeScript", "Python", "Aws", "PostgreSQL"],
+    imgSrc: yenn1,
+    projectScreenshots: [yenn1, yenn2, yenn3],
+    link: "https://blog-front-mauve-theta.vercel.app",
   },
   {
     name: "SupoGotchi",
@@ -40,15 +54,18 @@ export const projects: CarouselItemProps[] = [
     link: "https://github.com/aizuHackathon",
   },
   {
-    name: "Yenn",
-    description: `Yenn is an innovative blogging platform that stands at the intersection of simplicity and advanced technology. Initially created as a personal blog, the vision for Yenn is to eventually extend its reach to a broader user base. At its core, Yenn is designed to be extraordinarily user-friendly, ensuring that users can navigate to and read articles with minimal effort and maximum enjoyment.
-    The platform is built on a robust technical framework that combines React and TypeScript for a responsive and dynamic frontend. The backend is powered by Python FastAPI, known for its speed and reliability, while PostgreSQL ensures a stable and secure database. The integration of these technologies results in a seamless, fluid experience for the user.
-    A key feature of Yenn is its streamlined navigation, which allows readers to access content with fewer clicks, promoting a focused and uninterrupted reading experience. While currently serving as a personal blogging space, Yenn is constructed with scalability in mind, laying the groundwork for future expansion and community building.
-    Looking ahead, Yenn aspires to evolve from a personal platform into a vibrant, communal space for diverse stories and perspectives, all while staying true to its foundational principles of simplicity and user-centric design.`,
-    skills: ["React", "TypeScript", "Python", "Aws", "PostgreSQL"],
-    imgSrc: yenn1,
-    projectScreenshots: [yenn1, yenn2, yenn3],
-    link: "https://blog-front-mauve-theta.vercel.app",
+    name: "CleanAp",
+    description: `CleanAp is a cutting-edge mobile application specifically crafted to revolutionize the management of cleaning operations within a company. This internal tool is designed not only to schedule regular cleaning events but also to enhance the efficiency and engagement of the cleaning staff.
+      At the heart of CleanAp's innovation is its infrastructure; the app runs seamlessly on a company-owned server, ensuring data security and system reliability. It leverages the versatility of React Native and TypeScript for a responsive and user-friendly front-end, while Python powers the robust backend. This tech stack choice guarantees a smooth and consistent user experience across various devices and platforms.
+      One of the hallmark features of CleanAp is its containerized architecture. By embracing Docker, the application offers unparalleled ease in development and deployment processes. This containerization ensures that each component of the application is isolated, making updates and scaling hassle-free while maintaining a uniform environment across development and production.
+      Furthermore, CleanAp introduces an administrative game-changer by opting for a spreadsheet-based data management system. This approach demystifies the data handling experience, allowing non-technical administrators to easily monitor and manage the cleaning data without the complexity of traditional SQL databases. This spreadsheet integration means operations are more transparent, updates are instant, and training costs for admin personnel are significantly reduced.
+      But CleanAp isn't just about operational efficiency; it's also about employee motivation. The application incorporates a unique points system, where employees earn points that can be redeemed for various items like groceries within the company. This gamification aspect serves as an excellent incentive, fostering a more productive and engaged workforce.
+      In summary, CleanAp is not just a mobile application; it's a holistic solution for cleaning management that prioritizes ease of use, deployment, and maintenance, all while boosting employee morale and participation. It's a testament to how smart technology can be utilized to create a cleaner, more organized, and happier workplace.
+    `,
+    skills: ["React", "TypeScript", "Python", "Docker"],
+    imgSrc: cleanAp1,
+    projectScreenshots: [cleanAp1, cleanAp2, cleanAp3],
+    link: "https://www.figma.com/proto/G1DozIV4kDbdrxdXniTFfa/%E6%B8%85%E6%8E%83%E7%AE%A1%E7%90%86%E7%B0%BF%E3%82%A2%E3%83%97%E3%83%AA?type=design&node-id=294-5434&t=ZFJb83NSuaTbS1Fv-1&scaling=min-zoom&page-id=294%3A5415&starting-point-node-id=294%3A5733&mode=design",
   },
   {
     name: "Student Management System",
